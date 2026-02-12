3fc390c728c453b26f5ec9689cf13b02ae7c9642
pcAlejandro <eralex1000@gmail.com>
2026-02-12 11:48:43 +0100

    feat: ingreso nombre de animal

--- animals.py
+++ animals.py
@@ -4,6 +4,7 @@ animals = []
 # animals.append("Tigre de Ana")
 
 # ...
+animals.append("Leon de Alejandro")
 print("Hay", len(animals), "animales en el zoológico")
 for a in animals:
     print("-", a)
