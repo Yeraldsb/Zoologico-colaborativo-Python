0e59062e97fab53b2b9c578e1f391d94f9735fc5
Jeniree <jenireetovar@gmail.com>
2026-02-12 12:16:25 +0100

    feature/add-zorro-Jenireé

--- animals.py
+++ animals.py
@@ -4,6 +4,7 @@ animals = []
 # animals.append("Tigre de Ana")
 
 # ...
+animals.append("Zorro de Jenireé")
 print("Hay", len(animals), "animales en el zoológico")
 for a in animals:
     print("-", a)
