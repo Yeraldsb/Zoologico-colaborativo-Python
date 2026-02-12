d778cf6c667724708f4331bcc7f6a8cd58b39060
AGATA <agata.szlufcik@gmail.com>
2026-02-12 12:03:25 +0100

    feat: animal agregado

--- animals.py
+++ animals.py
@@ -1,9 +1,9 @@
 animals = []
-
 # Cada persona añade su animal aquí:
 # animals.append("Tigre de Ana")
 
 # ...
+animals.append ("Alpaca de Agata")
 print("Hay", len(animals), "animales en el zoológico")
 for a in animals:
     print("-", a)
