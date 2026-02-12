8723a5c69efde182a2d07a933594c36d09743cfe
Alain <allasa85@gmail.com>
2026-02-12 11:30:50 +0100

    animal favorito de Alain

--- animals.py
+++ animals.py
@@ -4,6 +4,8 @@ animals = []
 # animals.append("Tigre de Ana")
 
 # ...
+animals.append("Lobito de pelo gordo de Alain")
+
 print("Hay", len(animals), "animales en el zoológico")
 for a in animals:
     print("-", a)
