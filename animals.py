3d21eb175c181b1725c706d89eaf5a5cd547b39a
LuciaPelaez <lucygeo@gmail.com>
2026-02-12 11:56:21 +0100

    incluir mi animal a la lista

--- animals.py
+++ animals.py
@@ -4,6 +4,8 @@ animals = []
 # animals.append("Tigre de Ana")
 
 # ...
+animals.append("Koala de Lucia")
+
 print("Hay", len(animals), "animales en el zoológico")
 for a in animals:
     print("-", a)
