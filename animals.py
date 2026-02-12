2af759f4884136b12b2c691c1bc5dbc2f92b36de
josepuges <pascualpuges84@gmail.com>
2026-02-12 11:26:21 +0100

    Feat:Actualizacion del scrip

--- animals.py
+++ animals.py
@@ -1,9 +1,10 @@
 animals = []
 
 # Cada persona añade su animal aquí:
-animals.append("Suricato de Jose")
+# animals.append("Tigre de Ana")
 
 # ...
+animals.append("Tigre de Ana")
 print("Hay", len(animals), "animales en el zoológico")
 for a in animals:
     print("-", a)
