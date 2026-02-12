c97a418616d070dcb60f89c39b5d6c99b5a39ec2
Fracisco Jiménez <fcoxjp@gmail.com>
2026-02-12 11:46:52 +0100

    feat: agregado mi pajarito

--- animals.py
+++ animals.py
@@ -3,7 +3,7 @@ animals = []
 # Cada persona añade su animal aquí:
 # animals.append("Tigre de Ana")
 
-# ...
+animals.append("lechuza de Francisco") 
 print("Hay", len(animals), "animales en el zoológico")
 for a in animals:
     print("-", a)
