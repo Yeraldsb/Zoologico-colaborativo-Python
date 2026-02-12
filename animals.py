8346dc8feeaac2d5e3d463726fc31ad684fefcd4
Diana <diana.martinez.tech@gmail.com>
2026-02-12 14:28:05 +0100

    feat arreglo mi archivo

--- animals.py
+++ animals.py
@@ -2,7 +2,22 @@ animals = []
 # Cada persona añade su animal aquí:
 # animals.append("Tigre de Ana")
 # ...
+animals.append("Caballo de CPG")
+animals.append ("Alpaca de Agata")
+animals.append("Rata de Sana")
+animals.append("Lobito de pelo gordo de Alain")
+animals.append("Perro de Juan")
+animals.append("Michi de Diana")
+
+animals.append("Nutria de Michelle")
+animals.append("Oso Cheye")
+animals.append("Lobo de Sara") 
+animals.append("Delfín de Eva")
+
+animals.append("Koala de Lucy")
+
+animals.append("Nutria de Michelle")
+animals.append("Oso Cheye")
 print("Hay", len(animals), "animales en el zoológico")
 for a in animals:
-    print("-")
-
+    print("-", a)
