5338c2d1b9a7dcef0fd968b1e7ffb7e843d2d740
xuangfm <xuangfm@gmail.com>
2026-02-12 12:28:42 +0100

    perro de juan

--- animals.py
+++ animals.py
@@ -4,6 +4,8 @@ animals = []
 # animals.append("Tigre de Ana")
 
 # ...
+animals.append("Perro de Juan")
+
 print("Hay", len(animals), "animales en el zoológico")
 for a in animals:
     print("-", a)
