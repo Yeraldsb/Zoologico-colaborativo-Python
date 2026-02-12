f62a2adb4101d1d5665d973aa7f7aea0b9dc1582
Fracisco Jiménez <fcoxjp@gmail.com>
2026-02-12 14:51:07 +0100

    codigo sin comentarios

--- animals.py
+++ animals.py
@@ -1,8 +1,4 @@
 animals = []
-
-# Cada persona añade su animal aquí:
-# animals.append("Tigre de Ana")
-
 animals.append("lechuza de Francisco") 
 print("Hay", len(animals), "animales en el zoológico")
 for a in animals:
