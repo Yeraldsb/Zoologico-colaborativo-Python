b5516a78121d42768b0e181bb57f2bbdfc6c424b
PGCLAUDIO <pgclaudio72@gmail.com>
2026-02-12 11:27:24 +0100

    fute: Ingreso de animales

--- animals.py
+++ animals.py
@@ -4,6 +4,8 @@ animals = []
 # animals.append("Tigre de Ana")
 
 # ...
+animals.append("Tigre de Ana")
+
 print("Hay", len(animals), "animales en el zoológico")
 for a in animals:
     print("-", a)
