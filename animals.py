1cb112c27a5461cd4331ee06d25cc168dac46850
Sara Bailón <sarabailon593@hotmail.com>
2026-02-12 12:13:02 +0100

    Añadido animal de Sara debajo

--- animals.py
+++ animals.py
@@ -1,11 +1,12 @@
 animals = []
 
 # Cada persona añade su animal aquí:
-animals.append("Lobo de Sara") # animals.append("Tigre de Ana")
+# animals.append("Tigre de Ana")
 
 # ...
 animals.append("Nutria de Michelle")
 animals.append("Oso Cheye")
+animals.append("Lobo de Sara") 
 print("Hay", len(animals), "animales en el zoológico")
 for a in animals:
     print("-", a)
