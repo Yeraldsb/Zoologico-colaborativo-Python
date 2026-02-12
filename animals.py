9c3b8aaf9251ee67af662b17d091a4e8b4fcc078
Arantxa Sacacia <arantxamaquia@gmail.com>
2026-02-12 18:09:23 +0100

    Añadir animal a la lista

--- animals.py
+++ animals.py
@@ -2,19 +2,16 @@ animals = []
 # Cada persona añade su animal aquí:
 # animals.append("Tigre de Ana")
 # ...
-<<<<<<< HEAD
 animals.append ("Alpaca de Agata")
-=======
 animals.append("Rata de Sana")
 animals.append("Lobito de pelo gordo de Alain")
 animals.append("Perro de Juan")
-
 animals.append("Nutria de Michelle")
 animals.append("Oso Cheye")
 animals.append("Lobo de Sara") 
 animals.append("Delfín de Eva")
->>>>>>> origin/main
+animals.append("Leona de Arantxa")
 print("Hay", len(animals), "animales en el zoológico")
 for a in animals:
-    print("-")
+    print("-" , a)
 
