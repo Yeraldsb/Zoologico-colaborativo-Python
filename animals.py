d041b47fa535441885412d3c5b77b24a4348ea11
KrolinaTF <carolinatomasf@gmail.com>
2026-02-12 14:59:16 +0100

    feat:add-Camaleon-Carol

--- animals.py
+++ animals.py
@@ -12,6 +12,7 @@ animals.append("Nutria de Michelle")
 animals.append("Oso Cheye")
 animals.append("Lobo de Sara") 
 animals.append("Delfín de Eva")
+animals.append("Camaleón de Carol")
 
 print("Hay", len(animals), "animales en el zoológico")
 for a in animals:
