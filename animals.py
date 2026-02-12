00c28c1f482b8e312555750b4c796dae6e387e6d
yeraldsb <pamelasb9412@gmail.com>
2026-02-12 10:59:36 +0100

    feat: add animals.py with initial zoo script

---
+++ animals.py
@@ -0,0 +1,9 @@
+animals = []
+
+# Cada persona añade su animal aquí:
+# animals.append("Tigre de Ana")
+
+# ...
+print("Hay", len(animals), "animales en el zoológico")
+for a in animals:
+    print("-", a)
