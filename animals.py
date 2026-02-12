604af62e954f0944552d1be968ebc77be9a301a0
Lucía Llaneza <lu.llaneza@gmail.com>
2026-02-12 12:26:56 +0100

    feat: añadir gato montés de Lucía Ll.

--- animals.py
+++ animals.py
@@ -4,7 +4,11 @@ animals = []
 # animals.append("Tigre de Ana")
 
 # ...
+<<<<<<< HEAD
 animals.append("Gato montés de Lucía")
+=======
+animals.append("Nutria de Michelle")
+>>>>>>> d15cc4175d4bf06dd4278f65f5c8d8baad9a3726
 print("Hay", len(animals), "animales en el zoológico")
 for a in animals:
     print("-", a)
