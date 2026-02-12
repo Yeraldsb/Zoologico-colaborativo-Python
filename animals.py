a4cb38dd1b52015624d992ce336ecae816c3cddb
Ivan <asenjo_1976@hotmail.com>
2026-02-12 14:41:29 +0100

    desesperado

--- animals.py
+++ animals.py
@@ -9,16 +9,12 @@ animals.append ("Alpaca de Agata")
 animals.append("Rata de Sana")
 animals.append("Lobito de pelo gordo de Alain")
 animals.append("Perro de Juan")
-
->>>>>>> 28e8c7c1b2e16752c5046df3074ec4ad2dae513c
 animals.append("Nutria de Michelle")
 animals.append("Oso Cheye")
-gitanimals.append("Lobo de Sara") 
+animals.append("Lobo de Sara") 
 animals.append("Delfín de Eva")
->>>>>>> 6f75500c6ef4603aa370c86d321ce5d2136a3c05
->>>>>>> origin/main
 print("Hay", len(animals), "animales en el zoológico")
 for a in animals:
     print("-", a)
-    print("-")
+    
 
