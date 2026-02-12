47f683cf7179603645a5d7f80c8c5b61c0e7641f
Lucía Llaneza <lu.llaneza@gmail.com>
2026-02-12 15:02:28 +0100

    Añadir gato montes de Lucia Ll a la lista de animales

--- animals.py
+++ animals.py
@@ -1,10 +1,17 @@
 animals = []
-
 # Cada persona añade su animal aquí:
 # animals.append("Tigre de Ana")
-
 # ...
-animals.append("Gato montés de Lucía")
+animals.append("Caballo de CPG")
+animals.append ("Alpaca de Agata")
+animals.append("Rata de Sana")
+animals.append("Lobito de pelo gordo de Alain")
+animals.append("Perro de Juan")
+animals.append("Nutria de Michelle")
+animals.append("Oso Cheye")
+animals.append("Lobo de Sara") 
+animals.append("Delfín de Eva")
+animals.append("Gato montes de Lucia Ll.")
 print("Hay", len(animals), "animales en el zoológico")
 for a in animals:
     print("-", a)
