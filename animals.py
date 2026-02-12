b58b96cef1131529f6ce3543b7d18bf7f0cb764a
ArantxaPuig <apuigrod@gmail.com>
2026-02-12 19:58:21 +0100

    Resuelvo conflicto merge en animals.py

--- animals.py
+++ animals.py
@@ -4,12 +4,17 @@ animals = []
 # animals.append("Tigre de Ana")
 
 # ...
-<<<<<<< HEAD
+
 # animals.append("Colibrí de Arantxa")
 
 =======
 animals.append("Nutria de Michelle")
->>>>>>> d15cc4175d4bf06dd4278f65f5c8d8baad9a3726
+
 print("Hay", len(animals), "animales en el zoológico")
 for a in animals:
     print("-", a)
+
+# Nombre de mi colibri
+nombre = Arantxa
+print (nombre + "está cantando"
+       
