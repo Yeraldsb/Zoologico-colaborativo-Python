5dbda5859fee8f71c2cf16a42c7dc426d34121af
Lucía Llaneza <lu.llaneza@gmail.com>
2026-02-12 11:58:03 +0100

    Nuevo animal de Lucía en el zoológico

--- animals.py
+++ animals.py
@@ -4,7 +4,7 @@ animals = []
 # animals.append("Tigre de Ana")
 
 # ...
-animals.append("Gato montés")
+animals.append("Gato montés de Lucía")
 print("Hay", len(animals), "animales en el zoológico")
 for a in animals:
     print("-", a)
