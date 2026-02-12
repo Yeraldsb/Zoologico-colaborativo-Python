21d26f7d4a729c003b3978bbe8501ef159b68879
AGATA <agata.szlufcik@gmail.com>
2026-02-12 14:43:43 +0100

    Arreglando mi lista

--- animals.py
+++ animals.py
@@ -2,18 +2,14 @@ animals = []
 # Cada persona añade su animal aquí:
 # animals.append("Tigre de Ana")
 # ...
-<<<<<<< HEAD
 animals.append ("Alpaca de Agata")
-==
 animals.append("Rata de Sana")
-animals.append("Lobito de pelo gordo de Alain")git
+animals.append("Lobito de pelo gordo de Alain")
 animals.append("Perro de Juan")
-
 animals.append("Nutria de Michelle")
 animals.append("Oso Cheye")
 animals.append("Lobo de Sara") 
 animals.append("Delfín de Eva")
->>>>>>> origin/maingit 
 print("Hay", len(animals), "animales en el zoológico")
 for a in animals:
     print("-", a)
