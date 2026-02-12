042562b34e0830d07363496e28072331f53ac4d2
pcAlejandro <eralex1000@gmail.com>
2026-02-12 12:50:43 +0100

    feature: nombre de animal

--- animals.py
+++ animals.py
@@ -1,9 +1,10 @@
 animals = []
 
 # Cada persona añade su animal aquí:
-animals.append("Lobo de Sara") # animals.append("Tigre de Ana")
+# animals.append("Tigre de Ana")
 
 # ...
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 animals.append("Leon de Alejandro")
@@ -11,6 +12,11 @@ animals.append("Leon de Alejandro")
 animals.append("Nutria de Michelle")
 >>>>>>> d15cc4175d4bf06dd4278f65f5c8d8baad9a3726
 =======
+=======
+animals.append("Nutria de Michelle")
+animals.append("Oso Cheye")
+animals.append("Lobo de Sara") 
+>>>>>>> 6f75500c6ef4603aa370c86d321ce5d2136a3c05
 animals.append("Delfín de Eva")
 >>>>>>> b10b01b668eb829b50f889f692ce3ecfede12a82
 print("Hay", len(animals), "animales en el zoológico")
